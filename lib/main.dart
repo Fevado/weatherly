97742d85c9a80b19256f1f8067578375d0bcacca
favoured mwange <mwangefavoured@gmail.com>
2025-09-23 02:31:52 +0300

    Imports the dot env

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'screens/search_screen.dart';
 
-void main() {
+Future<void> main() async {
+  // Ensuring async binding
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Loading the env file
+  await dotenv.load(fileName: ".env");
+
   runApp(const ProviderScope(child: WeatherlyApp()));
 }
 
