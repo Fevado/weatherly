d8ca8ef2f68aba7d9dc0d11f2093ad28a8842aff
favoured mwange <mwangefavoured@gmail.com>
2025-09-25 01:15:26 +0300

    Updates the weather providers file

--- lib/providers/weather_providers.dart
+++ lib/providers/weather_providers.dart
@@ -1,22 +1,20 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-
 import '../models/weather.dart';
 import '../services/weather_service.dart';
 
-// Replace with your real API key
-final String apiKey = dotenv.env['API_KEY'] ?? "";
+// Read API key from --dart-define
+const String apiKey = String.fromEnvironment('API_KEY', defaultValue: "");
 
 // Service provider
 final weatherServiceProvider = Provider<WeatherService>((ref) {
+  if (apiKey.isEmpty) {
+    throw Exception("API_KEY not set");
+  }
   return WeatherService(apiKey);
 });
 
 // FutureProvider for async weather fetching by city
-final weatherProvider = FutureProvider.family<Weather, String>((
-  ref,
-  city,
-) async {
+final weatherProvider = FutureProvider.family<Weather, String>((ref, city) async {
   final service = ref.watch(weatherServiceProvider);
   return await service.fetchWeatherByCity(city);
 });
